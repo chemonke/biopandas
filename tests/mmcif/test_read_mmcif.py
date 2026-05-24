dccb93ef372f4b8ce841946342836ab2a54d18d8
chemonke <chemonke@pm.me>
2026-05-24 12:08:20 +0000

    fix missing value change in pandas 3 (None->nan)

--- tests/mmcif/test_read_mmcif.py
+++ tests/mmcif/test_read_mmcif.py
@@ -198,16 +198,16 @@ def test__construct_df():
             2,
             "ATOM",
             1,
-            None,
+            float("nan"),
             "A",
             "N",
             "SER",
             1,
             "23",
             1.0,
-            "None",
+            float("nan"),
             1,
-            None,
+            float("nan"),
             "N",
         ],
         index=[
@@ -239,7 +239,11 @@ def test__construct_df():
     # However, if I compare these values directly, they are equal.
     # assert exp.equals(dfs["ATOM"].loc[0, :])
     for k, v in exp.items():
-        assert dfs["ATOM"].loc[0, :][k] == v, k
+        actual = dfs["ATOM"].loc[0, :][k]
+        if pd.isna(v):
+            assert pd.isna(actual), k
+        else:
+            assert actual == v, k
 
 
 def test_read_pdb():
