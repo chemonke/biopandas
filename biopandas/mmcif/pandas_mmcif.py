89e961fcbbd93869395672e6a3a597e117db5c0d
chemonke <chemonke@pm.me>
2026-05-24 12:08:20 +0000

    fix missing value change in pandas 3 (None->nan)

--- biopandas/mmcif/pandas_mmcif.py
+++ biopandas/mmcif/pandas_mmcif.py
@@ -213,7 +213,9 @@ class PandasMmcif:
         full_df = pd.DataFrame.from_dict(
             data["atom_site"], orient="index"
         ).transpose()
+        mask = full_df.isna()
         full_df = full_df.astype(mmcif_col_types, errors="ignore")
+        full_df = full_df.where(~mask, other=np.nan)
         df["ATOM"] = pd.DataFrame(full_df[full_df.group_PDB == "ATOM"])
         df["HETATM"] = pd.DataFrame(full_df[full_df.group_PDB == "HETATM"])
         try:
